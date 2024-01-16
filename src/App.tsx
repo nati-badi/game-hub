f9c7339e5cefd05b8f34a48d8697415060de4266
BaDi <natibadigojimma@gmail.com>
2024-01-16 07:56:15 -0800

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="coral">
+      <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import logo from "../assets/pyramid.webp"
 function NavBar() {
   return (
     <HStack>
-        <Image src={logo} boxSize="60px" padding="5px" />
+        <Image src={logo} boxSize="60px" padding="7px" />
     </HStack>
   )
 }
